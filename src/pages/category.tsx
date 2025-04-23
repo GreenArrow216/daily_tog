6750fbfe082fa65c06a586645050507804da16a7
Green Arrow <arrowgreen021@gmail.com>
2025-04-23 11:54:13 +0100

    layout changed from column to row

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -1,15 +1,22 @@
 import { useParams } from "react-router-dom";
-import Gallery from "../widgets/gallery";
 import { categoryImages, imagesData } from "../constants";
+import PhotoGallery from "../widgets/photoGallery";
 
 const Category = () => {
   const { category } = useParams();
-  const images = imagesData.filter((img) => img.category === category);
+  const filteredImages = imagesData
+  .filter((img) => img.category === category)
+  .map((img) => ({
+    src: img.image_url,
+    width: parseInt(img.aspectRatio? img.aspectRatio.split(":")[0]:'1'),
+    height: parseInt(img.aspectRatio ? img.aspectRatio.split(":")[1] : '1'),
+  }));
 
+  
   return (
     <div>
-      <h1>{categoryImages.find(cat => cat.category === category)?.name}</h1>
-      <Gallery images={images} />
+      <h1>{categoryImages.find((cat) => cat.category === category)?.name}</h1>
+      <PhotoGallery images={filteredImages} containerWidth={window.innerWidth}/>
     </div>
   );
 };
---
+++ src/widgets/photoGallery.tsx
@@ -0,0 +1,61 @@
+import justifiedLayout from "justified-layout";
+import { useEffect, useState } from "react";
+
+interface Image {
+  src: string;
+  width: number;
+  height: number;
+}
+
+interface Props {
+  images: Image[];
+  containerWidth: number;
+  rowHeight?: number;
+  margin?: number;
+}
+
+const PhotoGallery = ({
+  images,
+  containerWidth,
+  rowHeight = 400,
+  margin = 8,
+}: Props) => {
+  const [layout, setLayout] = useState<ReturnType<typeof justifiedLayout>>();
+
+  useEffect(() => {
+    const aspectRatios = images.map((img) => img.width / img.height);
+    const geometry = justifiedLayout(aspectRatios, {
+      containerWidth,
+      targetRowHeight: rowHeight,
+      boxSpacing: margin,
+    });
+    setLayout(geometry);
+  }, [images, containerWidth, rowHeight, margin]);
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: containerWidth,
+        height: layout?.containerHeight || 0,
+      }}
+    >
+      {layout?.boxes.map((box, i: number) => (
+        <img
+          key={i}
+          src={images[i].src}
+          style={{
+            position: "absolute",
+            top: box.top,
+            left: box.left,
+            width: box.width,
+            height: box.height,
+            objectFit: "cover",
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PhotoGallery;
